c85626c14ec8cefdde96b8d3102abfdbaad63492
Mmaserufe Sekalo <maserufec@gmail.com>
2023-01-14 06:37:10 +0200

    added external files

---
+++ src/finalproject.js
@@ -0,0 +1,25 @@
+let now = new Date();
+let hour = now.getHours();
+if (hour < 10) {
+  hour = "0" + hour;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = "0" + minutes;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  " Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let time = hour + ":" + minutes;
+
+let dayElement = document.querySelector("#date");
+let timeElement = document.querySelector("#time");
+dayElement.innerHTML = day;
+timeElement.innerHTML = time;
