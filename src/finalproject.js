1ca46971df3dd2eeb49dfccdb88da9c3b6c61352
Mmaserufe Sekalo <maserufec@gmail.com>
2023-01-14 08:43:08 +0200

    add conversion link functions

--- src/finalproject.js
+++ src/finalproject.js
@@ -29,15 +29,27 @@ function displayTemperature(position) {
   let temperatureElement = document.querySelector("#current-temp");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind-speed")
+  let windElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#current-icon");
   cityElement.innerHTML = position.data.city;
   temperatureElement.innerHTML = Math.round(position.data.temperature.current);
   descriptionElement.innerHTML = position.data.condition.description;
   humidityElement = position.data.temperature.humidity;
   windElement.innerHTML = Math.round(position.data.wind.speed);
-  console.log(position.data.temperature.humidity);
-  console.log(position.data.wind.speed);
+  iconElement.setAttribute("src", position.data.condition.icon_url);
+
+  celsiusTemperature = position.data.temperature.current;
 }
+function searchCity(event) {
+  event.preventDefault();
+  let newCity = document.querySelector("#search").value;
+  let keys = "b4b16ao0bed60a37cdt0a5dcdf865c3b";
+  let searchApi = `https://api.shecodes.io/weather/v1/current?query=${newCity}&key=${keys}`;
+  axios.get(searchApi).then(displayTemperature);
+}
+
+let searchButton = document.querySelector("#search-input");
+searchButton.addEventListener("click", searchCity);
 
 function currentLocationData(current) {
   let city = current.data.city;
@@ -56,3 +68,21 @@ function currentLocation(position) {
 }
 
 navigator.geolocation.getCurrentPosition(currentLocation);
+
+function fahChange(event) {
+  event.preventDefault();
+  let imperialTemp = Math.round((celsiusTemperature * 9) / 5 + 32);
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = imperialTemp;
+}
+let celsiusTemperature = null;
+let fah = document.querySelector("#imperial");
+fah.addEventListener("click", fahChange);
+
+function celChange(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusChange = document.querySelector("#metric");
+celsiusChange.addEventListener("click", celChange);
